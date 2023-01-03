7beefd4d78c7278ea488a4eeb6a5b08d0612a4ce
falco <gyrovorbis@gmail.com>
2023-01-03 15:29:06 -0500

    Updated Lua module to use Lua 5.4.4.
    Lua 5.4.4 introduces a few nice features like to-be-closed variables,
    but most importantly it contains the new stuff like integer types from
    Lua 5.3 with performance improvements putting it much closer to Lua 5.1
    
    Additional GC characteristics have been improved, including an
    "emergency sweep" which happens when malloc() returns NULL and will
    be useful with the DC's limited RAM.

--- lua/files/KOSMakefile.mk
+++ lua/files/KOSMakefile.mk
@@ -1,16 +1,17 @@
 TARGET = liblua.a
-OBJS = src/lapi.o src/lcode.o src/lctype.o src/ldebug.o src/ldo.o src/ldump.o \
-	   src/lfunc.o src/lgc.o src/llex.o src/lmem.o src/lobject.o \
-	   src/lopcodes.o src/lparser.o src/lstate.o src/lstring.o src/ltable.o \
-	   src/ltm.o src/lundump.o src/lvm.o src/lzio.o \
-       src/lauxlib.o src/lbaselib.o src/lbitlib.o src/lcorolib.o src/ldblib.o \
-	   src/liolib.o src/lmathlib.o src/loslib.o src/lstrlib.o src/ltablib.o \
-	   src/lutf8lib.o src/loadlib.o src/linit.o
+OBJS = src/lapi.o src/lauxlib.o src/lbaselib.o src/lcode.o \
+	src/lcorolib.o src/lctype.o src/ldblib.o src/ldebug.o \
+	src/ldo.o src/ldump.o src/lfunc.o src/lgc.o src/linit.o \
+	src/liolib.o src/llex.o src/lmathlib.o src/lmem.o \
+	src/loadlib.o src/lobject.o src/lopcodes.o src/loslib.o \
+	src/lparser.o src/lstate.o src/lstring.o src/lstrlib.o \
+	src/ltable.o src/ltablib.o src/ltm.o src/lundump.o \
+	src/lutf8lib.o src/lvm.o src/lzio.o
 
 defaultall: fixconf $(OBJS) subdirs linklib
 
 fixconf:
-	sed -e 's/\/\* #define LUA_32BITS \*\//#define LUA_32BITS/' -ibak src/luaconf.h
+	sed -i 's/#define LUA_32BITS	0/#define LUA_32BITS	1/' src/luaconf.h
 
 KOS_CFLAGS += -Isrc
 
