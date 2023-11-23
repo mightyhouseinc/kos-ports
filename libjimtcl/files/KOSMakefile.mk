2f2f08647d6924b13f1d6509cfc641ac188bc80e
Spencer <s.elliott123@gmail.com>
2023-11-22 22:00:58 -0600

    Added support for jimtcl (#29)
    * Added support for jimtcl
    * Added GitHub handle for maintainer
    * Removed unused variables from Makefile
    * Fixed license type

---
+++ libjimtcl/files/KOSMakefile.mk
@@ -0,0 +1,18 @@
+PORTNAME =        libjimtcl
+CONFIGURE_ARGS =     --without-ext="aio,zlib"
+MAKE_TARGET =        libjim.a
+HDR_INSTDIR =        jimtcl
+
+AUTOTOOLS_HOST = sh-elf
+
+all: build-libjim fix-jimh
+
+build-libjim:
+	CC=kos-cc ${CONFIGURE_DEFS} ./configure --prefix=${KOS_PORTS}/${PORTNAME}/inst --host=${AUTOTOOLS_HOST} ${CONFIGURE_ARGS} ; \
+    	$(MAKE) ${MAKE_TARGET} 
+
+fix-jimh:
+	sed -i .bak "s@#include <jim-win32compat.h>@#include <${HDR_INSTDIR}/jim-win32compat.h>@g" jim.h
+	sed -i .bak "s@#include <jim-config.h>@#include <${HDR_INSTDIR}/jim-config.h>@g" jim.h
+
+include ${KOS_PORTS}/scripts/lib.mk
